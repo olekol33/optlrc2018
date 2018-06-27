727be38ff8b73af8c88991fc17310189e05d8ecb
olekol33 <olekol@gmail.com>
2018-06-27 11:57:28 +0300

    Update ErasureCodePluginOptLrc.h

--- src/erasure-code/optlrc/ErasureCodePluginOptLrc.h
+++ src/erasure-code/optlrc/ErasureCodePluginOptLrc.h
@@ -1,3 +1,21 @@
+/*
+***   Implementation of Optimal-LRC plugin for Ceph   ***
+
+Written by: Oleg Kolosov, Tel Aviv University
+Date: 27/06/2018
+
+The code for the implementation of USENIX ATC 2018 paper 
+## On Fault Tolerance, Locality, and Optimality in Locally Repairable Codes ###
+https://www.usenix.org/conference/atc18/presentation/kolosov
+
+Authors: 
+Oleg Kolosov, School of Electrical Engineering, Tel Aviv University; 
+Gala Yadgar, Computer Science Department, Technion and School of Electrical Engineering, Tel Aviv University; 
+Matan Liram, Computer Science Department, Technion; 
+Itzhak Tamo, School of Electrical Engineering, Tel Aviv University; 
+Alexander Barg, Department of ECE/ISR, University of Maryland
+*/
+
 #ifndef CEPH_ERASURE_CODE_PLUGIN_LRC_H
 #define CEPH_ERASURE_CODE_PLUGIN_LRC_H
 
